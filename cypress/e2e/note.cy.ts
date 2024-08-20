b86d023d929da3531f3e1395ca46a4902c0ac267
David <pairing4good@gmail.com>
2024-08-20 12:28:57 -0500

    completed step 9

--- cypress/e2e/note.cy.ts
+++ cypress/e2e/note.cy.ts
@@ -1,33 +1,43 @@
 beforeEach(() => {
-  cy.visit('/');
+  cy.visit("/");
 });
 
-describe('Note Capture', () => {
-  it('should create a note when name and description provided', () => {
-    cy.get('[data-testid=note-name-field]').type('test note');
-    cy.get('[data-testid=note-description-field]').type('test note description');
-    cy.get('[data-testid=note-form-submit]').click();
+after(() => {
+  cy.clearLocalStorage("notes");
+});
 
-    cy.get('[data-testid=note-name-field]').should('have.value', '');
-    cy.get('[data-testid=note-description-field]').should('have.value', '');
-    
-    cy.get('[data-testid=test-name-0]').should('have.text', 'test note');
-    cy.get('[data-testid=test-description-0]').should('have.text', 'test note description');
+describe("Note Capture", () => {
+  it("should create a note when name and description provided", () => {
+    cy.get("[data-testid=test-name-0]").should("not.exist");
+    cy.get("[data-testid=test-description-0]").should("not.exist");
+
+    cy.get("[data-testid=note-name-field]").type("test note");
+    cy.get("[data-testid=note-description-field]").type("test note description");
+    cy.get("[data-testid=note-form-submit]").click();
+
+    cy.get("[data-testid=note-name-field]").should("have.value", "");
+    cy.get("[data-testid=note-description-field]").should("have.value", "");
+
+    cy.get("[data-testid=test-name-0]").should("have.text", "test note");
+    cy.get("[data-testid=test-description-0]").should("have.text", "test note description");
   });
 });
 
-it('should create a note when name and description provided', () => {
-  cy.get('[data-testid=test-name-0]').should('not.exist');
-  cy.get('[data-testid=test-description-0]').should('not.exist');
+it("should load previously saved notes on browser refresh", () => {
+  cy.get("[data-testid=note-name-field]").type("test note 2");
+  cy.get("[data-testid=note-description-field]").type("test note description 2");
+  cy.get("[data-testid=note-form-submit]").click();
 
-  cy.get('[data-testid=note-name-field]').type('test note');
-  cy.get('[data-testid=note-description-field]').type('test note description');
-  cy.get('[data-testid=note-form-submit]').click();
 
-  cy.get('[data-testid=test-name-0]').should('have.text', 'test note');
-  cy.get('[data-testid=test-description-0]').should('have.text', 'test note description');
+  cy.get("[data-testid=test-name-0]").should("have.text", "test note 2");
+  cy.get("[data-testid=test-description-0]").should("have.text", "test note description 2");
+
+  cy.reload();
+
+  cy.get("[data-testid=test-name-0]").should("have.text", "test note 2");
+  cy.get("[data-testid=test-description-0]").should("have.text", "test note description 2");
 });
 
-it('should have header', () => {
-  cy.get('h1').should('have.text', 'My Notes App');
+it("should have header", () => {
+  cy.get("h1").should("have.text", "My Notes App");
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NoteForm from "./noteForm";
 import { Note } from "./types";
 import NoteList from "./noteList";
@@ -9,13 +9,32 @@ export default function App() {
   const [notes, setNotes] = useState<Note[]>([]);
   const [formData, setFormData] = useState<Note>({ name: "", description: "" });
 
+  useEffect(() => {
+    fetchNotesCallback();
+  }, []);
+
+  const fetchNotesCallback = () => {
+    const savedNotesString = localStorage.getItem("notes");
+    const savedNotes = savedNotesString ? JSON.parse(savedNotesString) : null;
+
+    if (savedNotes) return setNotes(savedNotes);
+    return setNotes([]);
+  };
+
+  const createNote = () => {
+    const updatedNoteList = [...notes, formData];
+    setNotes(updatedNoteList);
+    const updatedNotesListString = JSON.stringify(updatedNoteList);
+    localStorage.setItem('notes', updatedNotesListString);
+  };
+
   return (
     <>
       <NoteForm
         notes={notes}
         formData={formData}
         setFormDataCallback={setFormData}
-        setNotesCallback={setNotes}
+        setNotesCallback={createNote}
       />
       <NoteList notes={notes} />
     </>
