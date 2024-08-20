b21db92309e18977588b47570d44049ec854b3d0
David <pairing4good@gmail.com>
2024-08-20 12:32:53 -0500

    clean up redundant test

--- cypress/e2e/note.cy.ts
+++ cypress/e2e/note.cy.ts
@@ -4,6 +4,9 @@ beforeEach(() => {
 
 describe('Note Capture', () => {
   it('should create a note when name and description provided', () => {
+    cy.get('[data-testid=test-name-0]').should('not.exist');
+    cy.get('[data-testid=test-description-0]').should('not.exist');
+
     cy.get('[data-testid=note-name-field]').type('test note');
     cy.get('[data-testid=note-description-field]').type('test note description');
     cy.get('[data-testid=note-form-submit]').click();
@@ -11,18 +14,6 @@ describe('Note Capture', () => {
     cy.get('[data-testid=test-name-0]').should('have.text', 'test note');
     cy.get('[data-testid=test-description-0]').should('have.text', 'test note description');
   });
-});
-
-it('should create a note when name and description provided', () => {
-  cy.get('[data-testid=test-name-0]').should('not.exist');
-  cy.get('[data-testid=test-description-0]').should('not.exist');
-
-  cy.get('[data-testid=note-name-field]').type('test note');
-  cy.get('[data-testid=note-description-field]').type('test note description');
-  cy.get('[data-testid=note-form-submit]').click();
-
-  cy.get('[data-testid=test-name-0]').should('have.text', 'test note');
-  cy.get('[data-testid=test-description-0]').should('have.text', 'test note description');
 });
 
 it('should have header', () => {
