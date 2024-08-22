1fa5664b1b1b21c3b0f5e1c09b9d36e250d7b754
David <pairing4good@gmail.com>
2024-08-22 05:41:22 -0500

    fixed asynchronous issues

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -8,20 +8,21 @@ Amplify.configure(outputs);
 
 Cypress.Commands.add('signIn', () => { 
     const username = "pairing4good@gmail.com";
+    const password = "Pairing4good@"
     const userPoolClientId = outputs.auth.user_pool_client_id;
-    signIn({username, password: "Pairing4good@"})
+    const keyPrefix = `CognitoIdentityServiceProvider.${userPoolClientId}`;
+    
+    cy.wrap(signIn({ username, password}))
         .then((_signInOutput) => fetchAuthSession())
         .then((authSession) => {
             const idToken = authSession.tokens?.idToken?.toString() || ''
             const accessToken = authSession.tokens?.accessToken.toString() || ''
-            const keyPrefix = `CognitoIdentityServiceProvider.${userPoolClientId}`;
             
             cy.setLocalStorage(`${keyPrefix}.${username}.accessToken`, accessToken);
             cy.setLocalStorage(`${keyPrefix}.${username}.idToken`, idToken);
             cy.setLocalStorage(`CognitoIdentityServiceProvider.${userPoolClientId}.LastAuthUser`, username);
-        })
-        .catch((err) => console.log(err));
-    cy.saveLocalStorage();
+            cy.saveLocalStorage();
+        });
  })
 
 declare global {
